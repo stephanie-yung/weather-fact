0099e8c931c44567cc8e3ad164a4d6e37058c355
stephanie-yung <stephanie.yung43@myhunter.cuny.edu>
2021-11-06 23:35:49 -0400

    Initial Commit

---
+++ weather-fact/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-fact
+//
+//  Created by Stephanie Yung on 11/6/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
